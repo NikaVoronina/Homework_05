7411b56f3da54c4e0251a61308cdd55b7739c921
Ника Воронина <nika_voronina_95@mail.ru>
2022-08-24 21:40:06 +0300

    Task_3 added

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -3,15 +3,15 @@
 // Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 
 int[] array = new int[10];
-  for (int i = 0; i < array.Length; i++ )
-  {
+for (int i = 0; i < array.Length; i++ )
+{
     array[i] = new Random().Next(1, 10);
     Console.Write(array[i] + " ");
-  }
+}
 int max = array[0];
 int min = array[0];
-  for (int i = 1; i < array.Length; i++)
-  {
+for (int i = 1; i < array.Length; i++)
+{
     if (max < array[i])
     {
         max = array[i];
@@ -20,6 +20,6 @@ int min = array[0];
     {
         min = array[i];
     }
-  }
+}
 int decision = max - min;
 Console.WriteLine($"\nРазница между между максимальным -> {max} и минимальным -> {min} элементами массива = {decision}");
